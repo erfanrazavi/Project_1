22b65d5cc0cc820331d5400945323b2f5a083eaf
Erfan Razavi <erfan6235@gmail.com>
2024-07-21 21:52:37 +0330

    add static setting
    add load static to my all html file

--- index/urls.py
+++ index/urls.py
@@ -3,5 +3,9 @@ from index.views import *
 
 
 urlpatterns = [
-    path('' , index_views , name="index")
+    path('' , index_views , name="index"),
+    path('about/' , about_views , name="about"),
+    path('contact/' , contact_views , name="contact"),
+    path('elements/' , elements_views , name="element"),
+    path('blog/' , blog_views , name="blog"),
 ]
--- index/views.py
+++ index/views.py
@@ -5,3 +5,15 @@ from django.http import HttpResponse
 
 def index_views(request):
     return render(request , 'index.html')
+
+def about_views(request):
+    return render(request , 'about.html')
+
+def blog_views(request):
+    return render(request , 'blog.html')
+
+def contact_views(request):
+    return render(request , 'contact.html')
+
+def elements_views(request):
+    return render(request , 'elements.html')
