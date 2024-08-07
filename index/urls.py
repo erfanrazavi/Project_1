a91824ff649a4eb2f24b35b7468c61ec68a03734
Erfan Razavi <erfan6235@gmail.com>
2024-08-07 22:42:47 +0330

    Model forms and latest post in home page
    -add a category and title and content from db to latest posts
    -add model form to my index app
    -use model form in project template

--- index/urls.py
+++ index/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from index.views import *
+from blog.views import blog_home_views
 
 app_name = 'website'
 
@@ -7,6 +8,7 @@ urlpatterns = [
     path('' , index_views , name="index"),
     path('about/' , about_views , name="about"),
     path('contact/' , contact_views , name="contact"),
+    path('category/<str:blog_category>' , blog_home_views , name = 'category'),
     # path('elements/' , elements_views , name="element"),
     
 ]
--- index/views.py
+++ index/views.py
@@ -1,23 +1,46 @@
-from django.shortcuts import render , HttpResponse , get_object_or_404
+from django.shortcuts import render , HttpResponse , get_object_or_404 
 from django.http import HttpResponse , Http404
 from .models import Contact
+from django.core.paginator import Paginator ,PageNotAnInteger,EmptyPage
+from blog.models import BlogPost
+from index.forms import ContactForm
+from django.contrib import messages
+
 
 # Create your views here.
 
 
 def index_views(request):
-    return render(request , 'website/index.html')
+    posts = BlogPost.objects.filter(status=1)
+    paginator = Paginator(posts , 3)
+    page_number = request.GET.get('page')
+    
+    try:
+        posts = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts.paginator.page(paginator.num_pages)
+        
+    context = {'posts' : posts}
+    return render(request , 'website/index.html' , context)
 
 def about_views(request):
     return render(request , 'website/about.html')
 
 
 def contact_views(request):
-    # if request.method == 'POST':
-    #     form = Contact(request.POST)
-    #     if Contact.is_valid()
+    form = ContactForm(request.POST)
     
-    return render(request , 'website/contact.html')
+    if request.method == 'POST':
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            form.save()
+            messages.success(request, 'Dear %s! submission successful' % name)
+            print('done')
+            
+        
+    return render(request , 'website/contact.html' , {'form' : form})
 
 # def elements_views(request):
 #     return render(request , 'website/elements.html')
